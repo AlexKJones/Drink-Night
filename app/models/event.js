6110ab4eb0ac3a7b44da6de1bdb8906be6fcefcc
alexjones <akjones1992@yahoo.com>
2020-11-05 08:57:26 -0600

    changed examples to events curl scripts function
    all the files with example have been changed to event and I have the
    curl scripts for events working

---
+++ app/models/event.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
